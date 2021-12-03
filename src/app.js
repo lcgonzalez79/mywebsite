453c354167ea077f29f321d121a7772d56ce7e25
Raymundo Cornejo <rcornejo82@gmail.com>
2021-12-02 23:31:40 -0700

    second app for Heroku

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 //Define paths for Express config
 const publicDirectoryPath=path.join(__dirname,'../public')
